41eb8c3278fa1b73c3f7a89ee553dc9853d4a7ad
Michael Rubanov <mrubanov-rubanov@protonmail.com>
2018-02-19 22:54:58 +0200

    silent apt actions

--- nodejs-setup.zsh
+++ nodejs-setup.zsh
@@ -8,6 +8,7 @@ npm -v
 # yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
+sudo apt-get update -q2
+sudo apt-get install yarn -q2
 yarn -v
 yarn global add node-gyp forever pm2 babel-cli loopback-cli prettier
