05709012e16b52167d9e9976b9544b830203663a
Michael Rubanov <mrubanov-rubanov@protonmail.com>
2018-03-12 03:32:06 +0200

    .

--- storage-servers.zsh
+++ storage-servers.zsh
@@ -2,6 +2,8 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 #echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+sudo add-apt-repository 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main' -y
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update -q2
-sudo apt-get install -q2 postgresql redis-server redis-tools pgadmin3
+sudo apt-get install -q2 postgresql-10 redis-server redis-tools pgadmin3
 sudo apt-get install -y mongodb-org
