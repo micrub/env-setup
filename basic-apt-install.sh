ce4b9125799bf9008b1aa22a3eaf582fd910ee9c
Michael Rubanov <mrubanov-rubanov@protonmail.com>
2018-06-22 02:24:00 +0300

    compression pkg

--- basic-apt-install.sh
+++ basic-apt-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
 sudo apt-get update -q2
-sudo apt-get install git git-flow zsh vim keepass2 silversearcher-ag autojump htop sysstat pandoc lynx -q2
+sudo apt-get install p7zip-full git git-flow zsh vim keepass2 silversearcher-ag autojump htop sysstat pandoc lynx -q2
 sudo apt-get autoremove -q2
 sudo apt-get autoclean -q2
